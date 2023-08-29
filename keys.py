96c355956b82518c4590182c12180ead0fed1f2d
Sinyaev <sinyaev@skbe.ru>
2023-08-29 16:21:23 +0300

    Попытка 2

--- keys.py
+++ keys.py
@@ -1,6 +1,17 @@
 import pandas as pd
 from itertools import combinations
 
+
+def isPotentialKey(columns):
+    subset = data[list(columns)]
+    if len(subset) == len(subset.drop_duplicates()):
+        for key in candidate_keys:
+            if set(columns).issuperset(set(key)):
+                return False
+        return True
+    return False
+
+
 data = pd.read_excel('Table.xlsb', header=None, skiprows=1)
 data = data.drop(data.columns[0], axis=1)
 correct_column_names = data.iloc[0]
@@ -12,11 +23,11 @@ candidate_keys = []
 for num_columns in range(1, len(data.columns) + 1):
     column_combinations = combinations(data.columns, num_columns)
     for columns in column_combinations:
-        subset = data[list(columns)]
-
-        if len(subset) == len(subset.drop_duplicates()):
+        if isPotentialKey(columns):
             candidate_keys.append(columns)
+            print(columns)
 
 for key in candidate_keys:
     print("Candidate key:", key)
+
 print("Общее число candidate keys:", len(candidate_keys))
